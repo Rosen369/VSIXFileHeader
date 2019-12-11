b0f4a1e6f6c461f083346056cd835d5a66550fe9
rosen369 <rosen369@outlook.com>
2019-12-12 00:30:02 +0800

    set description in English

--- src/FileHeader/TemplateOptionPage.cs
+++ src/FileHeader/TemplateOptionPage.cs
@@ -34,7 +34,7 @@ namespace Rosen.FileHeader
 
         public TemplateOptionPage()
         {
-            this.FileHeaderTemplate = "/*******************************************************\r\n * \r\n * \r\n * FileName:$FileName$ \r\n * Author：Frank\r\n * CreateDate：$Today$\r\n * Description：此文件只包含一个类，具体内容见类型注释。$end$\r\n * Runtime：.NET 4.0\r\n * Version：1.0.0\r\n * \r\n * History：\r\n * CreateFile Frank $Now$\r\n * \r\n*******************************************************/\r\n\r\n";
+            this.FileHeaderTemplate = "/*******************************************************\r\n * \r\n * \r\n * FileName:$FileName$ \r\n * Author：Frank\r\n * CreateDate：$Today$\r\n * Description：This file contains only one class, for more details see type comments. $end$\r\n * Runtime：.NET 4.7\r\n * Version：1.0.0\r\n * \r\n * History：\r\n * CreateFile Frank $Now$\r\n * \r\n*******************************************************/\r\n\r\n";
         }
     }
 }
